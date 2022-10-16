2616ec5c5314d514b32baf73429b5d213ed4eed3
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-10-16 14:51:11 +0100

    import order

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Blog, User, Comment } = require("../models");
+const { User, Blog, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", (req, res) => {

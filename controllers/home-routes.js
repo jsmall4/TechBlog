33ddadf941fa60480fe68cda3264a2c7818a0ebb
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-10-16 15:19:15 +0100

    heroku fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,8 +3,6 @@ const sequelize = require("../config/connection");
 const { User, Blog, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-heroku 
-
 router.get("/", (req, res) => {
   Blog.findAll({
     include: [
